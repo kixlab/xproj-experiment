5421402893283711098d6ce9336ac800e96323b8
Hyunwoo Kim <khw0726@kaist.ac.kr>
2018-04-04 17:03:40 +0900

    budget Q

--- components/PromptPaneActionBudget.vue
+++ components/PromptPaneActionBudget.vue
@@ -4,7 +4,7 @@
       <b-button v-for="option in options" :key="option" @click="onBudgetClick(option)">{{option}}억원</b-button>
     </div>
     <div v-else>
-      이 공약에 실제 사용된 예산은 {{budget}}억원입니다. 
+      이 공약에 실제 사용된 예산은 {{budgetString}}입니다. 
       <br>
       <b-button @click="onNextClick" variant="primary">다음 질문 보기</b-button>
     </div>
@@ -15,6 +15,21 @@ export default {
   props: {
     options: Array,
     budget: Number
+  },
+  computed: {
+    budgetString: function () {
+      let int = Math.floor(budget)
+      let fraction = Math.floor((budget - int) * 1000)
+      if (int === 0 && fraction !== 0) {
+        return `${fraction}천만원`
+      } else if (int !== 0 && fraction === 0) {
+        return `${int}억원`
+      } else if (int !== 0 && fraction !== 0) {
+        return `${int}억 ${fraction}천만원`
+      } else {
+        return ''
+      }
+    }
   },
   data: function () {
     return {
