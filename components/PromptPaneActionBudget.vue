4729d332dacf7cb586e6d878eabfd944ddda4651
Hyunwoo Kim <khw0726@kaist.ac.kr>
2018-04-04 17:14:12 +0900

    margin fixed

--- components/PromptPaneActionBudget.vue
+++ components/PromptPaneActionBudget.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div v-if="!isAnswered">
-      <b-button v-for="option in options" :key="option" @click="onBudgetClick(option)">{{option}}억원</b-button>
+      <b-button class="budgetButton" v-for="option in options" :key="option" @click="onBudgetClick(option)" :variant="selectedBudget === option ? 'primary' : 'primary-outline'">{{option}}억원</b-button>
     </div>
     <div v-else>
       이 공약에 실제 사용된 예산은 {{budgetString}}입니다. 
       <br>
-      <b-button @click="onNextClick" variant="primary">다음 질문 보기</b-button>
+      <b-button class="nextButton" @click="onNextClick" variant="primary">다음 질문 보기</b-button>
     </div>
   </div>
 </template>
@@ -18,8 +18,8 @@ export default {
   },
   computed: {
     budgetString: function () {
-      let int = Math.floor(budget)
-      let fraction = Math.floor((budget - int) * 1000)
+      let int = Math.floor(this.budget)
+      let fraction = Math.floor((this.budget - int) * 1000)
       if (int === 0 && fraction !== 0) {
         return `${fraction}천만원`
       } else if (int !== 0 && fraction === 0) {
@@ -52,6 +52,10 @@ export default {
 
 .budgetButton {
   margin: 1em 0.5em;
+}
+
+.nextButton {
+  margin-top: 1em;
 }
 </style>
 
